9a514ae4cdd9933fcd065b8c35382a8424aa3e67
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-05-30 14:07:02 -0400

    fixing cors issue

--- backend/app.js
+++ backend/app.js
@@ -2,20 +2,17 @@ import express from "express";
 import cors from "cors";
 const app = express();
 
-// middleware
-app.use(cors({ origin: "https://moodyfoodz.pages.dev" }));
+const corsOptions = { origin: "https://moodyfoodz.pages.dev" };
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(express.json());
 
-// import any routes here as such
 import userPreferences from "./routes/getUserPref.route.js";
-
-//instantiate routes
 app.use("/api/v1/", userPreferences);
 
 app.get("/", (req, res) => {
-  res.json({
-    Server: "Welcome to MoodyFoodz",
-  });
+  res.json({ Server: "Welcome to MoodyFoodz" });
 });
 
 export default app;
